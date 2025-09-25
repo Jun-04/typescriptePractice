5ebc884d898dedbe8abbc2fe0013e663065f616e
jun <pinetreee.japan@gmail.com>
2025-09-25 16:27:51 +1000

    TypeScript Practie

---
+++ src/typeList.ts
@@ -0,0 +1,41 @@
+// // basic of TypeScript
+
+// // booloean
+// let bool: boolean = true;
+
+// // number
+// let num: number = 0;
+
+// // string
+// let str: string = "A";
+
+// // Array(2types) eiter way is ok bt it looks easy for me to think it's Array.
+// let arr1: Array<number> = [0, 1, 2];
+// let arr2: number[] = [0, 1, 2];
+
+// // tuple combo of number and stringf
+// let tuple: [number, string] = [0, "A"];
+
+// // any you can use this for anytype
+// let any1: any = false;
+
+// // void, if it doesn't return anyth, no need to write void because typescript
+// // sees it's void. ofc you can write it too.
+
+// // const funcA = () => {
+// //     const test = "TEST";
+// // };
+
+// const funcA = (): void => {
+//     const test = "TEST";
+// };
+
+// // null
+// let null1: null = null;
+
+// // undifined
+// let undefined1: undefined = undefined;
+
+// // obejct 2types
+// let obj1: object = {};
+// let obj2: { id: number, name: string } = { id: 0, name: "AAA"};
